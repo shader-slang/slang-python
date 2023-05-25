995827e7c5d239f7b1e4979304bbfd452266f6d7
Sai Praveen Bangaru <31557731+saipraveenb25@users.noreply.github.com>
2023-05-24 23:58:17 -0400

    Add ability to specify `-D` options in loadModule + fixes
     - Fixed issue with module name only being the first character.
     - Fixed issues with hyphens in slang file name.
     - Added hashing to ensure unique module name based on definition list.

--- slangpy.py
+++ slangpy.py
@@ -3,6 +3,9 @@ import sys
 import pkg_resources
 import subprocess
 import glob
+import hashlib
+import json
+import re
 
 from torch.utils.cpp_extension import load
 
@@ -22,9 +25,16 @@ else:
 slangc_path = os.path.join(
     package_dir, 'bin', 'slangc'+executable_extension)
 
-def _replaceFileExt(fileName, newExt):
+# If we have SLANGC_PATH set, use that instead
+if 'SLANGC_PATH' in os.environ:
+    slangc_path = os.environ['SLANGC_PATH']
+
+def _replaceFileExt(fileName, newExt, suffix=None):
     base_name, old_extension = os.path.splitext(fileName)
-    new_filename = base_name + newExt
+    if suffix:
+        new_filename = base_name + suffix + newExt
+    else:
+        new_filename = base_name + newExt
     return new_filename
 
 def find_cl():
@@ -51,21 +61,42 @@ def _add_msvc_to_env_var():
         if path_to_add not in os.environ["PATH"].split(os.pathsep):
             os.environ["PATH"] += os.pathsep + path_to_add
 
-def loadModule(fileName, skipSlang=False, verbose=False):
+def get_dictionary_hash(dictionary):
+    # Convert dictionary to JSON string
+    json_string = json.dumps(dictionary, sort_keys=True)
+
+    # Compute SHA-256 hash of the JSON string
+    hash_object = hashlib.sha256(json_string.encode())
+    hash_code = hash_object.hexdigest()
+
+    return hash_code
+
+def convert_non_alphanumeric_to_underscore(name):
+    converted_name = re.sub(r'\W+', '_', name)
+    return converted_name
+
+def loadModule(fileName, skipSlang=False, verbose=False, defines={}):
     if verbose:
         print("loading slang module: " + fileName)
         print("slangc location: " + slangc_path)
 
+    if defines:
+        options_hash = "-".join([get_dictionary_hash(defines)])
+    else:
+        options_hash = None
+    
     parent_folder = os.path.dirname(fileName)
     output_folder = os.path.join(parent_folder, ".slangpy_cache")
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     base_name = os.path.basename(fileName)
-    cppOutName = os.path.join(output_folder, _replaceFileExt(base_name, ".cpp"))
-    cudaOutName = os.path.join(output_folder, _replaceFileExt(base_name, "_cuda.cu"))
+    cppOutName = os.path.join(output_folder, _replaceFileExt(base_name, ".cpp", suffix=options_hash))
+    cudaOutName = os.path.join(output_folder, _replaceFileExt(base_name, "_cuda.cu", suffix=options_hash))
+
+    options = [f"-D{key}={value}" for (key, value) in defines.items()]
 
     if not(skipSlang and os.path.exists(cppOutName)):
-        result = subprocess.run([slangc_path, fileName, '-o', cppOutName, '-target', 'torch-binding' ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run([slangc_path, fileName, *options, '-o', cppOutName, '-target', 'torch-binding'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         slangcOutput = result.stderr.decode('utf-8')
         if slangcOutput.strip():
             print(slangcOutput)
@@ -73,14 +104,19 @@ def loadModule(fileName, skipSlang=False, verbose=False):
             raise RuntimeError(f"compilation failed with error {result.returncode}")
     
     if not(skipSlang and os.path.exists(cudaOutName)):
-        result = subprocess.run([slangc_path, fileName, '-o', cudaOutName ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run([slangc_path, fileName, *options, '-o', cudaOutName ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         slangcOutput = result.stderr.decode('utf-8')
         if slangcOutput.strip():
             print(slangcOutput)
         if result.returncode != 0:
             raise RuntimeError(f"compilation failed with error {result.returncode}")
     
-    moduleName = os.path.basename(fileName)[0]
+    baseModuleName = os.path.splitext(os.path.basename(fileName))[0]
+    if options_hash:
+        hash = hashlib.sha256("".join([baseModuleName, options_hash]).encode()).hexdigest()
+    else:
+        hash = hashlib.sha256(baseModuleName.encode()).hexdigest()
+    moduleName = "".join([convert_non_alphanumeric_to_underscore(baseModuleName), hash])
     
     # make sure to add cl.exe to PATH on windows so ninja can find it.
     _add_msvc_to_env_var()
